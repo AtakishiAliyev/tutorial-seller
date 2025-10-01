25ce282a81fbb8b5da8d9025d2e8f4d4625c4b96
RzayevTamerlan <tamerlan.rzev@gmail.com>
2025-10-01 11:44:26 +0400

    fix logo url

--- src/presentation/layouts/ui/Logo.tsx
+++ src/presentation/layouts/ui/Logo.tsx
@@ -6,7 +6,7 @@ const Logo = () => {
     <div className="flex items-center">
       <div className="flex-shrink-0">
         <Link to="/">
-          <img src="./aga-logo.jpeg" alt="Logo" className="w-[48px] h-[48px] rounded-full" />
+          <img src="/aga-logo.jpeg" alt="Logo" className="w-[48px] h-[48px] rounded-full" />
         </Link>
       </div>
     </div>
